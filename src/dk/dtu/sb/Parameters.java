ee75e6c59bc186263f6f7a8c584f8de1d6c28bf1
Jakob Jakobsen Boysen <jakob@jboysen.dk>
2013-11-04 15:31:12 +0100

    renamed parameters

--- src/dk/dtu/sb/Parameters.java
+++ src/dk/dtu/sb/Parameters.java
@@ -1,8 +1,8 @@
 package dk.dtu.sb;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -11,12 +11,11 @@ import java.util.Properties;
  */
 public class Parameters extends Properties {
     
-    private static final String PARAM_FILENAME = "INPUT_FILE";
-    public static final String PARAM_FILENAME_DEFAULT = "input.xml";
-    
+    private static final String PARAM_INPUT_FILENAME = "INPUT_FILE";
+    public static final String PARAM_INPUT_FILENAME_DEFAULT = "input.xml";    
 
-    private static final String PARAM_PARSER = "PARSER";
-    public static final String PARAM_PARSER_DEFAULT = "dk.dtu.sb.parser.SBMLParser";
+    private static final String PARAM_INPUT_PARSER = "INPUT_PARSER";
+    public static final String PARAM_INPUT_PARSER_DEFAULT = "dk.dtu.sb.parser.SBMLParser";
     
     private static final String PARAM_SIM_ALGORITHM = "SIM_ALGORITHM";
     public static final String PARAM_SIM_ALGORITHM_DEFAULT = "dk.dtu.sb.algorithm.GillespieAlgorithm";
@@ -45,17 +44,16 @@ public class Parameters extends Properties {
      */
     public Parameters(String filename) {
         this.setDefaults();
-        InputStream in = getClass().getResourceAsStream(filename);
         try {
-            this.load(in);
+            this.load(new FileInputStream(filename));
         } catch (IOException e) {
-            Util.log.fatal(e);
+            Util.log.fatal("An error occured when loading the properties file " + filename + ". Using defaults.");
         }
     }
     
     private void setDefaults() {
-        this.setFilename(PARAM_FILENAME_DEFAULT);
-        this.setParserClassName(PARAM_PARSER_DEFAULT);
+        this.setFilename(PARAM_INPUT_FILENAME_DEFAULT);
+        this.setParserClassName(PARAM_INPUT_PARSER_DEFAULT);
         
         this.setAlgorithmClassName(PARAM_SIM_ALGORITHM_DEFAULT);
         this.setIterations(PARAM_SIM_ITERATIONS_DEFAULT);
@@ -94,28 +92,28 @@ public class Parameters extends Properties {
      * @return The fully qualified class name of the parser impl.
      */
     public String getParserClassName() {
-        return this.getProperty(PARAM_PARSER, PARAM_PARSER_DEFAULT);
+        return this.getProperty(PARAM_INPUT_PARSER, PARAM_INPUT_PARSER_DEFAULT);
     }
     
     /**
      * @param className The fully qualified class name of the parser impl.
      */
     public void setParserClassName(String className) {
-       this.setProperty(PARAM_PARSER, className); 
+       this.setProperty(PARAM_INPUT_PARSER, className); 
     }
     
     /**
      * @return The name of the input file to the simulator
      */
     public String getFilename() {
-        return this.getProperty(PARAM_FILENAME, PARAM_FILENAME_DEFAULT);
+        return this.getProperty(PARAM_INPUT_FILENAME, PARAM_INPUT_FILENAME_DEFAULT);
     }
     
     /**
      * @param filename The name of the input file to the simulator
      */
     public void setFilename(String filename) {
-        this.setProperty(PARAM_FILENAME, filename);
+        this.setProperty(PARAM_INPUT_FILENAME, filename);
     }
     
     /**
