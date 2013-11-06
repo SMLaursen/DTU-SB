bb8ea145a999ea8ff2a88382bd3517c67ce79bfd
Jakob Jakobsen Boysen <jakob@jboysen.dk>
2013-11-06 15:32:06 +0100

    updated docs

--- src/dk/dtu/sb/data/Reaction.java
+++ src/dk/dtu/sb/data/Reaction.java
@@ -7,51 +7,58 @@ import java.util.Map;
  * Represents a transition in the {@link StochasticPetriNet}.
  */
 public class Reaction {
-    
+
     /**
      * The unique id of this reaction.
      */
     private String id;
-    
+
     /**
      * A human-readable label for this reaction.
      */
     private String name;
-    
+
     /**
      * 
      */
     private double rate;
-    
+
     /**
      * 
      */
     private double propensity;
-    
+
     /**
-     * 
+     * The reactants in this reactions.
      */
     private Map<String, Reactant> reactants = new HashMap<String, Reactant>();
-    
+
     /**
-     * 
+     * The products in this reaction.
      */
     private Map<String, Product> products = new HashMap<String, Product>();
-    
+
     /**
+     * Constructs a reaction.
      * 
      * @param id
+     *            See {@link #id}.
      * @param rate
+     *            See {@link #rate}.
      */
     public Reaction(String id, double rate) {
         this(id, id, rate);
     }
-    
+
     /**
+     * Constructs a reaction.
      * 
      * @param id
+     *            See {@link #id}.
      * @param name
+     *            See {@link #name}.
      * @param rate
+     *            See {@link #rate}.
      */
     public Reaction(String id, String name, double rate) {
         this.id = id;
@@ -59,26 +66,62 @@ public class Reaction {
         this.rate = rate;
     }
 
+    /**
+     * Add Reactant to this reaction.
+     * 
+     * @param reactant
+     *            See {@link Reactant}.
+     */
     public void addReactant(Reactant reactant) {
         reactants.put(reactant.getId(), reactant);
     }
 
+    /**
+     * Remove the Reactant from the reactants of this reaction.
+     * 
+     * @param reactantId
+     *            The unique id of the reactant.
+     * @return The removed {@link Reactant}.
+     */
     public Reactant removeReactant(String reactantId) {
         return reactants.remove(reactantId);
     }
 
+    /**
+     * Get all reactants of this reaction.
+     * 
+     * @return
+     */
     public Map<String, Reactant> getReactants() {
         return reactants;
     }
 
+    /**
+     * Add Product to this reaction.
+     * 
+     * @param product
+     *            See {@link Product}.
+     */
     public void addProduct(Product product) {
         products.put(product.getId(), product);
     }
 
+    /**
+     * Remove Product from the products of this reaction.
+     * 
+     * @param productId
+     *            The unique id of the product.
+     * @return The removed {@link Product}.
+     */
     public Product removeProduct(String productId) {
         return products.remove(productId);
     }
 
+    /**
+     * Get all products of this reaction.
+     * 
+     * @return
+     */
     public Map<String, Product> getProducts() {
         return products;
     }
@@ -89,18 +132,18 @@ public class Reaction {
     public String getName() {
         return name;
     }
-    
+
     /**
      * See {@link #id}.
      */
     public String getId() {
         return id;
     }
-    
+
     /**
      * Can be used to represent the reaction in e.g. graphs.
      * 
-     * @return {@link #name} if present, else {@link #id}. 
+     * @return {@link #name} if present, else {@link #id}.
      */
     public String getLabel() {
         return name != null ? name : id;
