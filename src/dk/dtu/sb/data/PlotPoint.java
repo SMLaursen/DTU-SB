4cefb635e2c84f954860a982abf49c6e6ec9fc4c
Jakob Jakobsen Boysen <jakob@jboysen.dk>
2013-12-16 12:00:01 +0100

    documentation

--- src/dk/dtu/sb/data/PlotPoint.java
+++ src/dk/dtu/sb/data/PlotPoint.java
@@ -2,6 +2,13 @@ package dk.dtu.sb.data;
 
 import java.util.Map;
 
+import dk.dtu.sb.output.AbstractOutputFormatter;
+
+/**
+ * This class holds plots (concentrations) for time points of final simulation.
+ * Typically this will be used in the
+ * {@link AbstractOutputFormatter#process(SimulationResult)} method.
+ */
 public class PlotPoint extends DataPoint<Float> {
 
     public PlotPoint(double time, Map<String, Float> markings) {
--- src/dk/dtu/sb/data/SimulationPoint.java
+++ src/dk/dtu/sb/data/SimulationPoint.java
@@ -2,6 +2,12 @@ package dk.dtu.sb.data;
 
 import java.util.Map;
 
+import dk.dtu.sb.algorithm.Algorithm;
+
+/**
+ * This class holds markings for time points when simulating. Typically this
+ * will be used in the {@link Algorithm#run()} method.
+ */
 public class SimulationPoint extends DataPoint<Integer> {
 
     public SimulationPoint(double time, Map<String, Integer> markings) {
