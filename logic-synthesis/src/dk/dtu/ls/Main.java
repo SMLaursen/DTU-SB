6451198880abb0c8fd22d14e7e7f14c204429303
Jakob Jakobsen Boysen <jakob@jboysen.dk>
2014-01-10 12:29:19 +0100

    updated ant file to work

--- logic-synthesis/src/dk/dtu/ls/Main.java
+++ logic-synthesis/src/dk/dtu/ls/Main.java
@@ -5,9 +5,13 @@ import dk.dtu.ls.database.DAO;
 public class Main {
 
     public static void main(String[] args) {
-        
-        // Setup database connection
         DAO.setup();
+        new Main();
+        DAO.close();
+    }
+    
+    private Main() {
         
     }
+    
 }
--- logic-synthesis/test/test/TestDatabase.java
+++ logic-synthesis/test/test/TestDatabase.java
@@ -2,7 +2,6 @@ package test;
 
 import static org.junit.Assert.*;
 
-import java.io.FileNotFoundException;
 import java.sql.SQLException;
 
 import org.junit.Test;
