1f97067aaadf5e6ec5bbb9e20d5fd8a5fb90e2de
Jakob Jakobsen Boysen <jakob@jboysen.dk>
2014-01-17 12:26:37 +0100

    added logic minimiser

--- logic-synthesis/README.md
+++ logic-synthesis/README.md
@@ -7,3 +7,7 @@
 
 For debugging purposes the database browser [SQLite Database Browser](http://sourceforge.net/projects/sqlitebrowser/) can be used
 to see the contents of the database in `/database/database.db`.
+
+# Dependencies
+
+* [Quine-McCluskey Logic Minimiser](https://github.com/qtstc/Circuit-Simulation/tree/master/Quine-McCluskey%20Java%20Sample%20Code)
---
+++ logic-synthesis/test/test/qmc/TestQMC.java
@@ -0,0 +1,31 @@
+package test.qmc;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.github.qtstc.Formula;
+
+public class TestQMC {
+
+    @Test
+    public void testSimple() throws IOException {
+        Formula f = Formula.read(new BufferedReader(new FileReader("test/test/qmc/input.txt")));
+        f.reduceToPrimeImplicants();
+        f.reducePrimeImplicantsToSubset();
+        System.out.println(f);
+    }
+    
+    @Test
+    public void testSimpleTT() throws IOException {
+        Formula f = Formula.readCompleteTT(new BufferedReader(new FileReader("test/test/qmc/input_tt.txt")));
+        f.reduceToPrimeImplicants();
+        f.reducePrimeImplicantsToSubset();
+        System.out.println(f);
+    }
+
+}
