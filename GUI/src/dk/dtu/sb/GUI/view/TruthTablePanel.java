8f9515db83275ff8ac64799aa6c3159b20082c6c
Jakob Jakobsen Boysen <jakob@jboysen.dk>
2014-01-29 22:52:30 +0100

    added info text, changed initial TT

--- GUI/src/dk/dtu/sb/GUI/view/TruthTablePanel.java
+++ GUI/src/dk/dtu/sb/GUI/view/TruthTablePanel.java
@@ -10,6 +10,8 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.SwingConstants;
 
 public class TruthTablePanel extends JPanel {
 
@@ -19,6 +21,8 @@ public class TruthTablePanel extends JPanel {
     public JTextArea textAreaMinimised;
     public JButton btnLoadSelectedDesign;
     public JList resultsList;
+    private JLabel lblTheLastColumn;
+    private JLabel lblRowsWithFalse;
 
     /**
      * Create the panel.
@@ -26,9 +30,9 @@ public class TruthTablePanel extends JPanel {
     public TruthTablePanel() {
         GridBagLayout gridBagLayout = new GridBagLayout();
         gridBagLayout.columnWidths = new int[] { 324, 0 };
-        gridBagLayout.rowHeights = new int[] { 29, 144, 29, 58, 27, 77, 0, 0 };
+        gridBagLayout.rowHeights = new int[] { 29, 0, 0, 144, 29, 58, 27, 77, 0, 0 };
         gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-        gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
                 4.9E-324 };
         setLayout(gridBagLayout);
 
@@ -38,15 +42,33 @@ public class TruthTablePanel extends JPanel {
         gbc_btnAddTruthTable.gridx = 0;
         gbc_btnAddTruthTable.gridy = 0;
         add(btnAddTruthTable, gbc_btnAddTruthTable);
+        
+        lblTheLastColumn = new JLabel("The last column is the output. ");
+        lblTheLastColumn.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+        GridBagConstraints gbc_lblTheLastColumn = new GridBagConstraints();
+        gbc_lblTheLastColumn.anchor = GridBagConstraints.WEST;
+        gbc_lblTheLastColumn.insets = new Insets(0, 0, 5, 0);
+        gbc_lblTheLastColumn.gridx = 0;
+        gbc_lblTheLastColumn.gridy = 1;
+        add(lblTheLastColumn, gbc_lblTheLastColumn);
+        
+        lblRowsWithFalse = new JLabel("Rows with false output can be omitted.");
+        lblRowsWithFalse.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+        GridBagConstraints gbc_lblRowsWithFalse = new GridBagConstraints();
+        gbc_lblRowsWithFalse.anchor = GridBagConstraints.WEST;
+        gbc_lblRowsWithFalse.insets = new Insets(0, 0, 5, 0);
+        gbc_lblRowsWithFalse.gridx = 0;
+        gbc_lblRowsWithFalse.gridy = 2;
+        add(lblRowsWithFalse, gbc_lblRowsWithFalse);
 
         truthTableRaw = new JTextArea();
         truthTableRaw.setFont(new Font("Lucida Console", Font.PLAIN, 13));
-        truthTableRaw.setText("aTc Ara CI\n000\n011\n101\n110");
+        truthTableRaw.setText("GFP IPTG lacI CI\n0001\n0011\n0101\n0111\n1000\n1010\n1100\n1111");
         GridBagConstraints gbc_truthTableRaw = new GridBagConstraints();
         gbc_truthTableRaw.fill = GridBagConstraints.BOTH;
         gbc_truthTableRaw.insets = new Insets(0, 0, 5, 0);
         gbc_truthTableRaw.gridx = 0;
-        gbc_truthTableRaw.gridy = 1;
+        gbc_truthTableRaw.gridy = 3;
         add(truthTableRaw, gbc_truthTableRaw);
         
         JPanel panel = new JPanel();
@@ -55,7 +77,7 @@ public class TruthTablePanel extends JPanel {
         gbc_panel.insets = new Insets(0, 0, 5, 0);
         gbc_panel.fill = GridBagConstraints.HORIZONTAL;
         gbc_panel.gridx = 0;
-        gbc_panel.gridy = 2;
+        gbc_panel.gridy = 4;
         add(panel, gbc_panel);
         
         btnMinimise = new JButton("Minimise");
@@ -70,7 +92,7 @@ public class TruthTablePanel extends JPanel {
         gbc_textArea.insets = new Insets(0, 0, 5, 0);
         gbc_textArea.fill = GridBagConstraints.BOTH;
         gbc_textArea.gridx = 0;
-        gbc_textArea.gridy = 3;
+        gbc_textArea.gridy = 5;
         add(textAreaMinimised, gbc_textArea);
 
         JLabel lblNewLabel = new JLabel("Results");
@@ -78,21 +100,21 @@ public class TruthTablePanel extends JPanel {
         gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
         gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
         gbc_lblNewLabel.gridx = 0;
-        gbc_lblNewLabel.gridy = 4;
+        gbc_lblNewLabel.gridy = 6;
         add(lblNewLabel, gbc_lblNewLabel);
         
         btnLoadSelectedDesign = new JButton("Load selected design");
         GridBagConstraints gbc_btnLoadSelectedDesign = new GridBagConstraints();
         gbc_btnLoadSelectedDesign.insets = new Insets(0, 0, 5, 0);
         gbc_btnLoadSelectedDesign.gridx = 0;
-        gbc_btnLoadSelectedDesign.gridy = 6;
+        gbc_btnLoadSelectedDesign.gridy = 8;
         add(btnLoadSelectedDesign, gbc_btnLoadSelectedDesign);
 
         JPanel resultDetailsPanel = new JPanel();
         GridBagConstraints gbc_resultDetailsPanel = new GridBagConstraints();
         gbc_resultDetailsPanel.fill = GridBagConstraints.BOTH;
         gbc_resultDetailsPanel.gridx = 0;
-        gbc_resultDetailsPanel.gridy = 7;
+        gbc_resultDetailsPanel.gridy = 9;
         add(resultDetailsPanel, gbc_resultDetailsPanel);
 
         resultsList = new JList();
@@ -100,7 +122,7 @@ public class TruthTablePanel extends JPanel {
         gbc_resultsList.insets = new Insets(0, 0, 5, 0);
         gbc_resultsList.fill = GridBagConstraints.BOTH;
         gbc_resultsList.gridx = 0;
-        gbc_resultsList.gridy = 5;
+        gbc_resultsList.gridy = 7;
         add(resultsList, gbc_resultsList);
     }
 
