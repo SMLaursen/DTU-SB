094c862b2f6eb95aaa4aa50b139caaf46ffba1fb
Sune <Sune1987@gmail.com>
2014-01-29 16:13:16 +0100

    Small test updates

--- logic-synthesis/test/test/technologymapping/TestTechMap.java
+++ logic-synthesis/test/test/technologymapping/TestTechMap.java
@@ -4,14 +4,12 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import dk.dtu.techmap.AIG;
 import dk.dtu.techmap.TechnologyMapper;
 
 public class TestTechMap {
-
 	AIG primary = new AIG("O = (C') + (A B)");
 
 	AIG part1 = new AIG("O = (I) + (C')","Part1");
@@ -19,12 +17,15 @@ public class TestTechMap {
 	AIG part3 = new AIG("I = (Q)","Part3");
 	AIG part4 = new AIG("O = (C') + (A B)","Part4");
 	AIG part5 = new AIG("Q = (A B)","Part5");
-
+	AIG part6 = new AIG("I = (A B) + (C' D)","Part6");
+	AIG part7 = new AIG("I = (Q B)","Part7");
+	AIG part8 = new AIG("Q = A","Part8");
+	//General purpose tests
+	
 	@Test
 	public void test1(){	
-		//Test that proteins have to match
-		
 		HashSet<AIG> library = new HashSet<AIG>();
+		
 		//Test that p1 and p5 cannot be a solution
 		library.add(part1);
 		library.add(part5);
@@ -52,24 +53,23 @@ public class TestTechMap {
 		//Test that the previous tries didn't cause any side-effects
 		library.remove(part3);
 		library.remove(part4);
+		library.add(part6);
 		solution = techmap.start(library);
 		assertTrue(solution == null);	
 	}
-
+	
+	//Advanced currently failing tests
 	@Test
-	public void test2(){
-		//Create library
+	public void test2(){	
 		HashSet<AIG> library = new HashSet<AIG>();
-		library.add(part1);
-		//				library.add(part2);
-		library.add(part3);
-		//				library.add(part4);
-		library.add(part5);
-		//Define technology mapper on primary part
 		TechnologyMapper techmap = new TechnologyMapper(primary);
+		
+		library.add(part1);
+		library.add(part7);
+		library.add(part8);
 		HashSet<AIG> solution = techmap.start(library);
 		System.out.println(solution);
 	}
-
-
+	
+	
 }
