771f709d552e6e24afdf07922e97ed9982df8824
Sune <Sune1987@gmail.com>
2014-01-30 14:04:43 +0100

    Improved techmapper

--- logic-synthesis/src/dk/dtu/techmap/TechnologyMapper.java
+++ logic-synthesis/src/dk/dtu/techmap/TechnologyMapper.java
@@ -17,7 +17,7 @@ public class TechnologyMapper {
 		//Add top-level node
 		graph = g;
 	}
-	
+
 	/** Starts mapping.
 	 * @return
 	 * null : No mapping could be made
@@ -28,34 +28,32 @@ public class TechnologyMapper {
 		HashSet<SBGate> solution = new HashSet<SBGate>();
 		return map(solution, startingGate);
 	}
-	
+
 	/** Maps using the library of {@link AIG}'s
 	 * 
 	 * @return
 	 * null : no match could be found
 	 * Set  : the set of parts that make up the match*/
 	private HashSet<SBGate> map(HashSet<SBGate> selectedParts, HashMap<String,LogicGate> toMatch){
-		
+
 		//For each incomplete matching
 		for(String protein : toMatch.keySet()){
 			LogicGate g = toMatch.get(protein);
-			
-			//Try to match it using any part from library
+
+			//Try to match it using any part from library with matching proteins
 			for(SBGate sbGate : Library.getGatesWithOutput(protein)){
-			    AIG libPart = sbGate.getAIG();
-			    
+				AIG libPart = sbGate.getAIG();
+
 				//Ensure orthogonality
 				if(selectedParts.contains(sbGate)){
 					continue;
 				}
-				
-//				//Ensure proteins match
-				/*if(!libPart.getOutputGate().getProtein().equals(protein)){
-					continue;
-				}*/
-				
+
+				//TODO Ensure complete orthogonality by checking all intermediate proteins
+
 				//Can this be libPart be matched?
 				HashMap<String, LogicGate> toMatchNext = isMatching(libPart.getOutputGate(), g);
+
 				//Part didn't match. skip this part.
 				if(toMatchNext==null){
 					continue;
@@ -98,114 +96,93 @@ public class TechnologyMapper {
 	 * */
 	public HashMap<String, LogicGate> isMatching(LogicGate other, LogicGate atPos){
 		//Used as control : null = no match, empty = complete match 
-		//Values = partial match i.e. need to continue explore these vales with other parts
-		HashMap<String, LogicGate> nextMatches = new HashMap<String, LogicGate>();
+		//Values = partial match i.e. need to continue explore these vales with other part
+		if(other instanceof AndGate && atPos instanceof AndGate){
 
-		if(other instanceof AndGate){
-			if(atPos instanceof AndGate){
-				//Map for storing intersections
-				HashMap<String, LogicGate> mergedMatches = new HashMap<String, LogicGate>();
-				//Sanity check
-				assert(atPos.in.size() == 2);
-				assert(other.in.size() == 2);
+			//Map for storing intersections
+			HashMap<String, LogicGate> mergedMatches = new HashMap<String, LogicGate>();
 
-				//Counts the numbers of branches that are matching
-				int match = 0;
+			//Sanity Checks
+			assert(other.in.size() == 2);
+			assert(atPos.in.size() == 2);
 
-				//TODO : Gives false positives
+			LogicGate thisChild1 = (LogicGate) atPos.in.toArray()[0];
+			LogicGate thisChild2 = (LogicGate) atPos.in.toArray()[1];
+			LogicGate otherChild1 = (LogicGate) other.in.toArray()[0];
+			LogicGate otherChild2 = (LogicGate) other.in.toArray()[1];
 
-				//To remember if a branch already has been matched.
-				LogicGate alreadyMatched = null;
+			HashMap<String, LogicGate> nextMap1 = new HashMap<String, LogicGate>();
+			HashMap<String, LogicGate> nextMap2 = new HashMap<String, LogicGate>();
 
-				//Check children
-				for(LogicGate childThis : atPos.in){
-					for(LogicGate childOther : other.in){
-						//If this branch has already been matched, then skip it.
-						if(alreadyMatched != null && alreadyMatched == childOther){
-							continue;
-						}
-						else if(childOther.getClass() == childThis.getClass()){
-							nextMatches = isMatching(childOther,childThis);
-							//See if it matches downwards.
-							if(nextMatches != null){
-								//Found one match 
-								match++;
-								alreadyMatched = childOther;
-								mergedMatches.putAll(nextMatches);
-								//If both branches could be matched, return their union.
-								if(match == 2){
-									return mergedMatches;
-								} 
-								//Else break (And check the other branch)
-								break;
-							}
-						}
-					}
+			//Try combination 1:
+			nextMap1 = isMatching(otherChild1,thisChild1);
+			if(nextMap1 != null){
+				nextMap2 = isMatching(otherChild2,thisChild2);
+				if(nextMap2 != null){
+					mergedMatches.putAll(nextMap1);
+					mergedMatches.putAll(nextMap2);
+					return mergedMatches;
 				}
-				//return null when every branch has been tried matched.
-				return null;
 			}
-
-		} else if(other instanceof NotGate){
-			if(atPos instanceof NotGate){
-				//Sanity checks
-				assert(other.in.size() == 1);
-				assert(atPos.in.size() == 1);
-				//Recurse on both children
-				LogicGate childOther = (LogicGate) other.in.toArray()[0];
-				LogicGate childThis = (LogicGate) atPos.in.toArray()[0];
-				return isMatching(childOther, childThis);
+			//Try combination 2:
+			nextMap1 = isMatching(otherChild1,thisChild2);
+			if(nextMap1 != null){
+				nextMap2 = isMatching(otherChild2,thisChild1);
+				if(nextMap2 != null){
+					mergedMatches.putAll(nextMap1);
+					mergedMatches.putAll(nextMap2);
+					return mergedMatches;
+				}
 			}
-			//Else no match
+			//No matches has been found
 			return null;
-			
+
+		} else if(other instanceof NotGate && atPos instanceof NotGate){
+			//Sanity checks
+			assert(other.in.size() == 1);
+			assert(atPos.in.size() == 1);
+			//Recurse on both children
+			LogicGate childOther = (LogicGate) other.in.toArray()[0];
+			LogicGate childThis = (LogicGate) atPos.in.toArray()[0];
+			return isMatching(childOther, childThis);
+
 		} else if(other instanceof InputGate){
+			HashMap<String, LogicGate> nextMatches = new HashMap<String, LogicGate>();
 			if(atPos instanceof InputGate){
-				//If proteins match - and are specified in the inMap of the AIG
-				if(((InputGate) other).getProtein().equals(((InputGate) atPos).getProtein()) 
-						&& graph.containsInputProtein(((InputGate) atPos).getProtein())){
+				//If others protein 
+				if(!graph.containsInputProtein(((InputGate) other).getProtein())){
+					nextMatches.put(((InputGate) other).getProtein(), atPos);
+					return nextMatches;
+				}//If proteins match - and are specified in the inMap of the AIG
+				else if(((InputGate) other).getProtein().equals(((InputGate) atPos).getProtein())){
 					//nextMatches should be empty here
 					assert(nextMatches.isEmpty());
 					return nextMatches;
-				}
+				} 
 				else{
 					//Proteins do not match
-					//TODO add functionality to translate proteins freely
 					return null;
 				}
-			}
-			else{
+			}else{
 				//Continue from this node using other's protein 
 				nextMatches.put(((InputGate) other).getProtein(), atPos);
 				return nextMatches;
 			}
-			
-//			if(!(atPos instanceof InputGate && other.toString().equals(atPos.toString()) )){
-//				//Otherwise store position of graph where to resume matching next part
-//				//If not wrong mappings! (Orthogonality ensures we can abort here)
-//				if(graph.inMap.containsKey(atPos.toString())){
-//					return null;
-//				} else {
-//					nextMatches.put(other.toString(),atPos);
-//				}
-//			}
-//			return nextMatches;
-		
 		} else if(other instanceof OutputGate){
 			//Sanity check
 			assert(other.in.size() == 1);
 			//Proceed to next node
 			LogicGate childOther = (LogicGate) other.in.toArray()[0];
 			LogicGate childThis = atPos;
+			//If also a OutputGate (Only happens on the first match)
 			if(atPos instanceof OutputGate){
 				assert(atPos.in.size() == 1);
 				childThis = (LogicGate) atPos.in.toArray()[0];
 			}
 			return isMatching(childOther, childThis);
-		
+
 		} else {
-			throw new RuntimeException("Unrecognized graph structure");
+			return null;
 		}
-		return null;
 	}
 }
